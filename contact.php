3e02b7cc9a28b770f7b1090938160a727a573921
apham8 <98186481+apham8@users.noreply.github.com>
2022-11-30 17:47:52 -1000

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,40 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
-    <link rel="manifest" href="/site.webmanifest">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <!-- Navigation bar -->
-  <nav id="navbar">
-      <div class="container">
-          <a href="dashboard.php" style="text-decoration: none">
-                  <div class="logo">
-                      <h1><span class="smarter">Smarter</span> <span class="money">Money</span></h1>
-                  </div>
-              </a>
-          <div class="menu-icon">
-              <i class='bx bx-menu' id="menu-icon"></i>
-          </div>
-          <div class="nav">
-              <ul>
-                  <li><a href="dashboard.php">Home</a></li>
-                  <li><a href="about.php">About Us</a></li>
-                  <li><a href="contact.php">Contact</a></li>
-                  <li><a href="logout.php">Log out</a></li>
-              </ul>
-          </div>
-      </div>  
-  </nav>
-  <!-- Main section -->
-    <h2 style="text-align:center">Contact</h2>
+<?php 
+include_once 'header.php';
+ ?>
+    <h1 style="text-align:center">Contact</h1>
       <p>If you have any questions, please contact one of this emails :</p>
       <p>tnguyen10@my.hpu.edu</p>
       <p>apham8@my.hpu.edu</p>
-</body>
-</html>
+      <?php 
+      include_once 'footer.php';
+       ?>

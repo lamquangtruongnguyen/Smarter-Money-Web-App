f4c89e2bdaba0f2cc3604e21c12efc51e4a610b8
apham8 <98186481+apham8@users.noreply.github.com>
2022-11-30 14:57:47 -1000

    Update contact.php

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@
       </div>  
   </nav>
   <!-- Main section -->
-    <h1 style="text-align:center">Contact</h1
+    <h1> style="text-align:center">Contact</h1>
       <p>If you have any questions, please contact one of this emails :</p>
       <p>tnguyen10@my.hpu.edu</p>
       <p>apham8@my.hpu.edu</p>

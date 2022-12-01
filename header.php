e8686093df897fa384d6867e8951d6e81b674ab7
apham8 <98186481+apham8@users.noreply.github.com>
2022-11-30 18:16:44 -1000

    Update header.php

--- header.php
+++ header.php
@@ -1,37 +1,44 @@
+<?php
+    session_start();    
+    include('./includePHP/config.php');
+    if(strlen($_SESSION["userlogin"]) == 0) {
+        header("location: index.php");
+    }
+    else {
+?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>SmarterMoney - Money Management App</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <!-- Navigation bar -->
-        <nav id="navbar">
-            <div class="container">
-                <a href="index.php" style="text-decoration: none">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmarterMoney - Money Management App</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- Navigation bar -->
+    <nav id="navbar">
+        <div class="container">
+            <a href="dashboard.php" style="text-decoration: none">
                     <div class="logo">
                         <h1><span class="smarter">Smarter</span> <span class="money">Money</span></h1>
                     </div>
                 </a>
-                <div class="menu-icon">
-                    <i class='bx bx-menu' id="menu-icon"></i>
-                </div>
-                <div class="nav">
-                    <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="about.php">About Us</a></li>
-                        <li><a href="signup.php">Sign Up</a></li>
-                        <li><a href="contact.php">Contact</a></li>
-                    </ul>
-                </div>
-
+            <div class="menu-icon">
+                <i class='bx bx-menu' id="menu-icon"></i>
             </div>
-        </nav>
+            <div class="nav">
+                <ul>
+                    <li><a href="dashboard.php">Home</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="logout.php">Log out</a></li>
+                </ul>
+            </div>
+        </div>  
+    </nav>

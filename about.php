b854f404b3f1642241e38e0c20518b7356eb82cc
apham8 <98186481+apham8@users.noreply.github.com>
2022-11-30 17:53:27 -1000

    Update about.php

--- about.php
+++ about.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+include_once 'header.php';
+ ?>
+
+  <!-- Main section -->
+    <h1 style="text-align:center">About us</h1
+      <p>This is a project created for Database technologies class</p>        
+      <p>Created by :</p>
+      <p>Truong Nguyen</p>
+      <p>Anh Pham</p>
+
+<?php 
+include_once 'footer.php';
+?>

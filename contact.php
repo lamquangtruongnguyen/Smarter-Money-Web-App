2da816a7c87dffa53914b518b4bf24f85cdcc78d
apham8 <98186481+apham8@users.noreply.github.com>
2022-12-03 02:44:24 -1000

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
 <?php 
 include_once 'header.php';
+<link rel="stylesheet" href="css/contactstyle.css">
  ?>
     <h1 style="text-align:center">Contact</h1>
       <p>If you have any questions, please contact one of this emails :</p>

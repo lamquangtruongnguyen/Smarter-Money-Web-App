460dd95d2b86d555643cbe2937f90533ffc546d5
Truong Nguyen <77467939+lamquangtruongnguyen@users.noreply.github.com>
2022-12-07 19:42:47 -1000

    Basic content and styling

---
+++ aboutus.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        a {
+            text-decoration: none;
+            color: white;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body class="bg-light">
+    <div class="container text-center text-white w-50 h-25 bg-success position-absolute top-50 start-50 translate-middle d-flex flex-column justify-content-center">
+        <h1 class="h1 mb-0">Work is on the way</h1>
+        <a class="fs-3" href="./dashboard.php"><u>Back to Homepage</u></a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
